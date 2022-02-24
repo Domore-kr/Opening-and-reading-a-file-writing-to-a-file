771112472a8d747d033efaa520b4cee91fde2eb8
Domore-kr <8749132@mail.ru>
2022-02-24 18:22:13 +0500

    Revorking

--- Task-three.py
+++ Task-three.py
@@ -1,20 +1,25 @@
 import os
 
-way = os.path.join(os.getcwd(), 'text')
-lst = []
-for i in os.listdir(way):
-    if i.endswith('.txt'):
-        with open(os.path.join(os.getcwd(), 'text', i)) as read_list:
-            dct = {'Имя файла': i, 'Данные файла': read_list.readlines()}
-            dct['Количество строк'] = len(dct['Данные файла'])
-            lst.append(dct)
 
-with open('sorted_text.txt', 'a', encoding='utf-8') as sorted_text:
-    for i in sorted(lst, key=lambda x: x['Данные файла'], reverse=True):
-        sorted_text.write(i['Имя файла'])
-        sorted_text.write('\n')
-        sorted_text.write(str(i['Количество строк']))
-        sorted_text.write('\n')
-        for a in i['Данные файла']:
-            sorted_text.write(str(a).strip())
+def sort_txt(folder, sort):
+    way = os.path.join(os.getcwd(), folder)
+    lst = []
+    for i in os.listdir(way):
+        if i.endswith('.txt'):
+            with open(os.path.join(os.getcwd(), 'text', i)) as read_list:
+                dct = {'Имя файла': i, 'Данные файла': read_list.readlines()}
+                dct['Количество строк'] = len(dct['Данные файла'])
+                lst.append(dct)
+
+    with open(sort, 'a', encoding='utf-8') as sorted_text:
+        for i in sorted(lst, key=lambda x: x['Данные файла'], reverse=True):
+            sorted_text.write(i['Имя файла'])
             sorted_text.write('\n')
+            sorted_text.write(str(i['Количество строк']))
+            sorted_text.write('\n')
+            for a in i['Данные файла']:
+                sorted_text.write(str(a).strip())
+                sorted_text.write('\n')
+
+
+sort_txt('text', 'sorted_text.txt')
--- Task_one-two.py
+++ Task_one-two.py
@@ -1,19 +1,22 @@
-cook_book = {}
-with open('recipes.txt') as cook:
-    for i in cook:
-        cook_book[i.strip()] = []
-        for a in range(int(cook.readline())):
-            b = cook.readline().strip()
-            cook_book[i.strip()].append(
-                {'ingredient_name': b[0:b.find('|')], 'quantity': int(b[b.find('|') + 2: b.rfind('|') - 1]),
-                 'measure': b[b.rfind('|') + 2:]})
-        cook.readline()
+def cook_book(rec):
+    cook_book = {}
+    with open(rec) as cook:
+        for i in cook:
+            cook_book[i.strip()] = []
+            for a in range(int(cook.readline())):
+                b = cook.readline().strip()
+                cook_book[i.strip()].append(
+                    {'ingredient_name': b[0:b.find('|')], 'quantity': int(b[b.find('|') + 2: b.rfind('|') - 1]),
+                     'measure': b[b.rfind('|') + 2:]})
+            cook.readline()
+        return cook_book
 
 
-def get_shop_list_by_dishes(dishes, person_count):
+def get_shop_list_by_dishes(dishes, person_count, rec):
     ingridients = {}
+    cook = cook_book(rec)
     for i in dishes:
-        for a in cook_book[i]:
+        for a in cook[i]:
             if ingridients.get(a['ingredient_name']) is None:
                 ingridients[a['ingredient_name']] = {'measure': a['measure'],
                                                      'quantity': int(a['quantity'] * person_count)}
@@ -24,4 +27,5 @@ def get_shop_list_by_dishes(dishes, person_count):
                                                              double_ingridient + int(a['quantity']) * person_count)}
     return print(ingridients)
 
-get_shop_list_by_dishes(['Омлет', 'Фахитос', 'Омлет'], 2)
+
+get_shop_list_by_dishes(['Омлет', 'Фахитос', 'Омлет'], 2, 'recipes.txt')
